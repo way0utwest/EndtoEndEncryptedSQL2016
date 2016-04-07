f624822a64575e2c03dcdef65816c8f17576c7fc
way0utwest <sjones@dkranch.net>
2016-04-07 10:04:12 -0600

    Changes to setup code
    Added header and cleaned up some scripts

--- SQLCode/00_endtoend_initialsetup.sql
+++ SQLCode/00_endtoend_initialsetup.sql
@@ -1,4 +1,16 @@
-create database EncryptionDemo;
+/*
+End to End Encryption - Setup
+
+
+Steve Jones, copyright 2016
+
+This code is provided as is for demonstration purposes. It may not be suitable for
+your environment. Please test this on your own systems. This code may not be republished 
+or redistributed by anyone without permission.
+You are free to use this code inside of your own organization.
+*/
+IF NOT EXISTS( SELECT name FROM sys.databases AS d WHERE name = 'EncryptionDemo')
+  CREATE database EncryptionDemo;
 go
 -- create login AEUser
 use EncryptionDemo
